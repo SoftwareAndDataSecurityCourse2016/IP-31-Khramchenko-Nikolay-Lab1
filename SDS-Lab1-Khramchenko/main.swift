7f57de5d14b918b872406820ab050e73cc5786d9
Nikolay Khramchenko <Developer@developer.local>
2016-09-22 13:10:52 +0300

    Initial Commit

---
+++ SDS-Lab1-Khramchenko/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SDS-Lab1-Khramchenko
+//
+//  Created by Nikolay Khramchenko on 9/22/16.
+//  Copyright © 2016 KPI. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
